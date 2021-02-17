63f70565867855b72afa0827fb05d6790ae6b9c0
anna <76838137+izcoser@users.noreply.github.com>
2021-02-17 18:26:14 -0300

    Add more collision points to pipes

--- Flappy/pipe.cpp
+++ Flappy/pipe.cpp
@@ -39,8 +39,28 @@ void Pipe::change(void){
         bottom.y = top.y;
         top.y = aux;
     }
+    else if(top.y - bottom.y <= 0.20){
+        bottom.y -= 0.2;
+    }
 
-    width = (((float) (rand() % 5)) + 1) / 10.0f;
+    width = (((float) (rand() % 50)) + 10) / 100.0f;
+    this->update_points();
 
     printf("New bottom.y, top.y, width: %.2f, %.2f, %.2f\n", bottom.y, top.y, width);
+}
+
+void Pipe::update_points(void){
+    points[0].x = bottom.x;                 points[0].y = 0;
+    points[1].x = bottom.x;                 points[1].y = bottom.y / 2;
+    points[2].x = bottom.x + width / 2;     points[2].y = bottom.y;
+    points[3].x = bottom.x + width;         points[3].y = bottom.y;
+    points[4].x = bottom.x + width;         points[4].y = bottom.y / 2;
+    points[5].x = bottom.x + width;         points[5].y = 0;
+
+    points[6].x = top.x;                    points[6].y = 1;
+    points[7].x = top.x;                    points[7].y = (top.y + 1) / 2;
+    points[8].x = top.x + width / 2;        points[8].y = top.y;
+    points[9].x = top.x + width;            points[9].y = top.y;
+    points[10].x = top.x + width;           points[10].y = (top.y + 1) / 2;
+    points[11].x = top.x + width;           points[11].y = 1;
 }
--- Flappy/pipe.h
+++ Flappy/pipe.h
@@ -1,5 +1,6 @@
 #ifndef PIPE_H
 #define PIPE_H
+#define PIPE_POINTS 12
 
 #include "point.h"
 
@@ -9,9 +10,12 @@ class Pipe{
         Point top;
         float width;
 
+        Point points[PIPE_POINTS];
+
         Pipe(Point _bottom, Point _top, float _width);
         void draw(void);
         void change(void);
+        void update_points(void);
 };
 
 #endif
